6852af750d64ae1f20d3ba8457097c2650c54030
developer-2023 <132809345+developer-2023@users.noreply.github.com>
2023-11-05 21:05:23 +0100

    Initial commit

---
+++ 008-capitalize.py
@@ -0,0 +1,13 @@
+# docs.python.org/3/library/stdtypes.html#string-methods
+
+# Ask user for their name
+name = input("What's your name? ")
+
+# Remove whitespace from str
+name = name.strip()
+
+# Capitalize user's name
+name = name.capitalize()
+
+# Say hello to user
+print(f"hello, {name}!")
